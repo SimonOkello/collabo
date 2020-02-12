962a03bcdcaeddb7aa7984ada9c8697248649390
nokash <ashfordnokash@gmail.com>
2020-02-12 14:52:23 +0300

    Login Profile Review SignUp views.py

--- jasim/views.py
+++ jasim/views.py
@@ -3,15 +3,32 @@ from django.http import HttpResponse
 from . import views
 # Create your views here.
 
+
 def index(request):
     return render(request, 'index.html', {})
+
+
+def signup(request):
+    return render(request, 'signup.html', {})
+
+
+def login(request):
+    return render(request, 'login.html', {})
+
+
+def profile(request):
+    return render(request, 'profile.html', {})
+
+
+def project(request):
+    return render(request, 'project.html', {})
+
 
 def detail(request):
     return render(request, 'portfolio-detail.html', {})
 
-def signup(request):
-    return HttpResponse("This is signup page")
 
-def login(request):
-    return HttpResponse("This is login page")
+def review(request):
+    return render(request, 'review.html', {})
+
 
