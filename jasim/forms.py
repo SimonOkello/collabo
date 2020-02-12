4e3ccc4390255fde8cba90cebedd5cee74854ed8
Imma7 <immamugambi@gmail.com>
2020-02-12 15:06:42 +0300

    Create SignUp form

--- jasim/forms.py
+++ jasim/forms.py
@@ -3,3 +3,8 @@ from django import forms
 class LoginForm(forms.Form):
   email = forms.EmailField(label='Email')
   password = forms.CharField(label='Password')
+
+class SignupForm(forms.Form):
+  username = forms.CharField(label='Username')
+  email = forms.EmailField(label='Email')
+  password = forms.CharField(label='Password')
