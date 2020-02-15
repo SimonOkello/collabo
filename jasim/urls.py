f639d3a112ab8e56c3b65fbc2a90f44eeec38419
SimonOkello <simonokello93@gmail.com>
2020-02-15 18:25:57 +0300

    Change project && detail urls to class urls

--- jasim/urls.py
+++ jasim/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path('profile/', views.profile, name='profile'),
-    path('project/', views.project, name='project'),
-    path('detail/', views.detail, name='detail'),
+    path('project/', views.ProjectList.as_view(), name='project'),
+    path('project/<int:pk>/', views.ProjectDetail.as_view(), name='detail'),
     path('review/', views.review, name='review'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login')
